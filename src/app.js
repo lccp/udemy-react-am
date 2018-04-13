08c1ef44c5a91a5a08a603108448112401abc985
Lucas Cavalcante <lucas.cavalcante@ifactory.com.br>
2018-04-13 13:51:51 -0300

    configured webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
-console.log('app.js is running');
+// import './utils';
+// import subtract, { square, add } from './utils';
+
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(4, 10));
+// console.log(subtract(4, 10));
+
+import isSenior, { isAdult, canDrink } from './person';
+
+console.log(isAdult(18));
+console.log(canDrink(18));
+console.log(isSenior(64));
---
+++ src/person.js
@@ -0,0 +1,7 @@
+
+
+const isAdult = (age) => age >= 18;
+const canDrink = (age) => age >= 18;
+const isSenior = (age) => age >= 50;
+
+export { isAdult, canDrink, isSenior as default}
